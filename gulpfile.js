834533625b470e64d1aedd5be7921a934edcab4a
Alekseev Konstantin <alexcseev1990@gmail.com>
2021-03-02 09:06:46 +0200

    fc

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,14 @@ const imagemin = require("gulp-imagemin");
 const rename = require("gulp-rename");
 
 //Порядок подключения файлов со стилями
-const styleFiles = ["./src/css/color.scss", "./src/css/main.scss"];
+const styleFiles = [
+	"./src/css/color.scss",
+	"./src/css/main.scss",
+	"./src/css/fonts.scss",
+	"./src/css/footer.scss",
+	"./src/css/first-section.scss",
+	"./src/css/second-section.scss"
+];
 //Порядок подключения js файлов
 const scriptFiles = ["./src/js/lib.js", "./src/js/main.js"];
 
@@ -37,11 +44,11 @@ gulp.task("styles", () => {
 	return (
 		gulp
 			.src(styleFiles)
-			.pipe(sourcemaps.init())
-			//Указать stylus() , sass() или less()
-			.pipe(sass())
+			// .pipe(sourcemaps.init())
 			//Объединение файлов в один
 			.pipe(concat("style.css"))
+			//Указать stylus() , sass() или less()
+			.pipe(sass())
 			//Добавить префиксы
 			.pipe(
 				autoprefixer({
@@ -49,12 +56,12 @@ gulp.task("styles", () => {
 				}),
 			)
 			//Минификация CSS
-			.pipe(
-				cleanCSS({
-					level: 2,
-				}),
-			)
-			.pipe(sourcemaps.write("./"))
+			// .pipe(
+			// 	cleanCSS({
+			// 		level: 2,
+			// 	}),
+			// )
+			// .pipe(sourcemaps.write("./"))
 			// .pipe(
 			// 	rename({
 			// 		suffix: ".min",
@@ -62,6 +69,15 @@ gulp.task("styles", () => {
 			// )
 			//Выходная папка для стилей
 			.pipe(gulp.dest("./build/css"))
+			.pipe(browserSync.stream())
+	);
+});
+
+gulp.task("fonts", () => {
+	return (
+		gulp
+			.src("./src/fonts/**")
+			.pipe(gulp.dest("./build/fonts/"))
 			.pipe(browserSync.stream())
 	);
 });
@@ -101,12 +117,13 @@ gulp.task("del", () => {
 gulp.task("img-compress", () => {
 	return gulp
 		.src("./src/img/**")
-		.pipe(
-			imagemin({
-				progressive: true,
-			}),
-		)
-		.pipe(gulp.dest("./build/img/"));
+		// .pipe(
+		// 	imagemin({
+		// 		progressive: true,
+		// 	}),
+		// )
+		.pipe(gulp.dest("./build/img/"))
+		.pipe(browserSync.stream())
 });
 
 //Таск для отслеживания изменений в файлах
@@ -118,6 +135,7 @@ gulp.task("watch", () => {
 	});
 	//Следить за добавлением новых изображений
 	gulp.watch("./src/img/**", gulp.series("img-compress"));
+	gulp.watch("./src/fonts/**", gulp.series("fonts"));
 	//Следить за файлами со стилями с нужным расширением
 	gulp.watch("./src/css/**/*.scss", gulp.series("styles"));
 	//Следить за JS файлами
@@ -129,5 +147,5 @@ gulp.task("watch", () => {
 //Таск по умолчанию, Запускает del, styles, scripts, img-compress и watch
 gulp.task(
 	"default",
-	gulp.series("del", gulp.parallel("styles", "scripts", "img-compress"), "watch"),
+	gulp.series("del", gulp.parallel("styles", "scripts", "img-compress", "fonts"), "watch"),
 );
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,39 @@
-console.log("Hello world2");
+$(document).ready(function(){
+    $('.slider').slick({
+        centerMode: true,
+        // centerPadding: '60px',
+        slidesToShow: 3,
+        dots: true,
+        variableWidth: true
+        // responsive: [
+        //     {
+        //         breakpoint: 768,
+        //         settings: {
+        //             arrows: false,
+        //             centerMode: true,
+        //             centerPadding: '40px',
+        //             slidesToShow: 3
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 480,
+        //         settings: {
+        //             arrows: false,
+        //             centerMode: true,
+        //             centerPadding: '40px',
+        //             slidesToShow: 1
+        //         }
+        //     }
+        // ]
+    });
+
+    // $('.slider').on('swipe', function(event, slick, direction){
+    //     console.log(direction);
+    //     // left
+    //   });
+    console.log($(".slick-active").siblings())
+    $('.slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+        console.log(currentSlide);
+    });
+});
+
